65c3f70f21cdc32c96e18091122b746657064f39
Tozo sensei <codersensei@outlook.com>
2024-06-07 12:34:56 -0700

    fix(routing): handle undefined routes with NotFound component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import NotFound from "./components/NotFound";
+import "./assets/styles.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = React.lazy(() => import("./App"));
 
 
-import NotFound from './components/NotFound';
-import './assets/styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-        {/* <BrowserRouter>
-    <Routes>
-    <Route path="/"  element={<App />} />
-      <Route path="about"  element={<AboutMe />} />
-      <Route path="contact"  element={<ContactMe />} />
-      <Route path="project"  element={<Project />} />
-      <Route path="*" element={<NotFound />} />
+    <Router>
+    <Suspense>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter> */}
-    <App />    
+      </Suspense>
+    </Router>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
